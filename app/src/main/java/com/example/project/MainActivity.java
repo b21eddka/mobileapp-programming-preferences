635e7413a6df9a7343da95c0ecf418c4a95ed817
b21eddka <b21eddka@student.his.se>
2022-05-05 22:41:40 +0200

    SharedPreference lades till samt att datan inte försvinner efter appens nedstänging

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.project;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +22,29 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        textView = findViewById(R.id.textView);
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE);
+        String textInput = sharedPreferences.getString("textInput", "");
+
+        textView.setText(textInput);
+
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity2();
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
             }
         });
     }
 
-    public void openActivity2() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE);
+        String textInput = sharedPreferences.getString("textInput", "");
 
-        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-        startActivity(intent);
+        textView.setText(textInput);
     }
-
 }
--- app/src/main/java/com/example/project/MainActivity2.java
+++ app/src/main/java/com/example/project/MainActivity2.java
@@ -1,14 +1,47 @@
 package com.example.project;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    EditText editText;
+    Button saveButton;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        editText = findViewById(R.id.editText);
+        saveButton = findViewById(R.id.saveButton);
+        sharedPreferences = getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        textView = findViewById(R.id.textView);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                editor = sharedPreferences.edit();
+                editor.putString("textInput", editText.getText().toString());
+                editor.commit();
+                editor.apply();
+            }
+        });
     }
+
+
+
 }
